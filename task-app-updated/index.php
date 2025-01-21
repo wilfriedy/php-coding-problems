4a33723bad490606128501d9ab3deece11792c69
wilfriedy <wilfriedakpe3@gmail.com>
2025-01-21 22:25:39 +0100

    updated task app with marking task as complete

--- task-app-updated/index.php
+++ task-app-updated/index.php
@@ -27,12 +27,28 @@
         echo "All fields are required";
     } else {
         $openedFile = fopen($myTaskFile,'a');
-        fwrite($openedFile, $taskTitle . " - priority : $priority" . "\n");
+        fwrite($openedFile, "[ ] - " . $taskTitle . " - " . $priority . "\n");
         fclose($openedFile);
         $_POST = [];
         header("Location: " . $_SERVER["PHP_SELF"]);
     }
 
+ }
+
+
+ if(isset($_GET["done"])){
+    $indx = $_GET["done"];
+    $tasksList = file($myTaskFile, FILE_IGNORE_NEW_LINES);
+
+    // check if the current done value is a valid index in the array
+    if(isset($tasksList[$indx])){
+        // mark the [ ] as [ x ] creating a toggle effect 
+        $tasksList[$indx] =  str_contains($tasksList[$indx], "[ ]") ? str_replace("[ ]", "[ x ]", $tasksList[$indx]) : str_replace("[ x ]", "[ ]", $tasksList[$indx]);
+        //update the tasks list with the new value
+        $updatedList = implode("\n", $tasksList); // implode joins each item in the list with a new line
+        file_put_contents($myTaskFile, $updatedList . "\n"); // file put contents will write the entire data to file
+        header("Location: " . $_SERVER["PHP_SELF"]); // refresh the page to remove the values from the urkl
+    }
  }
 
 ?>
@@ -63,6 +79,15 @@
         .task-container{
             margin-top: 20px;
         }
+        .task-item{
+            display: flex;
+            flex-direction:row;
+            justify-content:space-between;
+            font-size: 20px;
+            padding: 10px 0;
+            margin-bottom: 10px;
+            width: 200px;
+        }
     </style>
 </head>
 <body>
@@ -82,7 +107,10 @@
             <?php if(count($allTasks) > 0) :?>
                 <ul>     
                     <?php foreach($allTasks as $key => $value) :?>
-                        <li><?= $value ?></li>
+                        <?php
+                            $newValue = explode("-", $value);
+                        ?>
+                        <li class="task-item"> <a href="?done=<?=$key?>"><?= $newValue[0]?></a> <?= $newValue[1]?> <b><?= $newValue[2]?></b></li>
                     <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
